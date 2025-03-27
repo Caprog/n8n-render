01e9b502f871bb9f5c14d17c7f765d6dbcaa9fdd
Claudio <caprog.info@gmail.com>
2025-03-27 00:11:14 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 set -e
 while [ ! -d "/home/node/.n8n" ]; do sleep 1; done
 touch /home/node/.n8n/database.sqlite
+ls -lh /home/node/.n8n
 exec n8n --sqlite="/home/node/.n8n/database.sqlite"
+ls -lh /home/node/.n8n
